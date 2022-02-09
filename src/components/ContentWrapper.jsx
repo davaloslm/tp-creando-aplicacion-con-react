342c2c652772b56e1a9f0c524825021552f7211d
DávalosLeonardo <davaloslm@hotmail.com.ar>
2022-02-09 03:48:24 -0300

    Agrega ContentWrapper

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import TopBar from './TopBar'
+import ContentRowTop from './ContentRowTop'
+import Footer from './Footer'
 
 function ContentWrapper() {
     return (
         <div>
-            
+            <TopBar/>
+            <ContentRowTop/>
+            <Footer/>
         </div>
     )
 }
