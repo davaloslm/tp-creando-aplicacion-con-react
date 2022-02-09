3c3dfb367b475813f1e98554155d3bbf61045af9
DávalosLeonardo <davaloslm@hotmail.com.ar>
2022-02-09 03:40:29 -0300

    Crea componentes, agrega assets y corrige importaciones

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/css/app.css"
+
+function App() {
+    return (
+        <div className='App'>
+            
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContentWrapper() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ContentWrapper
