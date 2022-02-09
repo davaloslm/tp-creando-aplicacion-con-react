ec537d333ecdf1a49ddccc3671fdf2c0fc281e0c
DávalosLeonardo <davaloslm@hotmail.com.ar>
2022-02-09 04:08:02 -0300

    Agrega TopBar, ContentRowTop y Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import ContentWrapper from "./ContentWrapper"
 
 function App() {
     return (
-        <div className='App'>
+        <div className='App muestra'>
             <SideBar/>
             <ContentWrapper/>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            
+            <footer className="sticky-footer bg-white">
+				<div className="container my-auto">
+					<div className="copyright text-center my-auto">
+						<span>Copyright &copy; Dashboard 2021</span>
+					</div>
+				</div>
+			</footer>
         </div>
     )
 }
